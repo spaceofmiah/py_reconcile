7046fd5c4212fa11f957df28d7e395cf0a65c01e
spaceofmiah <agbonzeosazuwa@gmail.com>
2024-03-12 18:09:53 +0100

    handle: key error on discrepancy check

--- csv_reconciler.py
+++ csv_reconciler.py
@@ -29,9 +29,13 @@ def store_field_discrepancies(source, target, store, key, message) -> int:
     count = 0
     for i, row in source.iterrows():
         for column in source.columns:
-            if row[column] != target.loc[i, column] and row[key] == target_df.loc[i, key]:
-                count += 1
-                store.append((message, row[column], target_df.loc[i, column]))
+            try:
+                if row[column] != target.loc[i, column] and row[key] == target_df.loc[i, key]:
+                    count += 1
+                    store.append((message, row[column], target_df.loc[i, column]))
+            except KeyError:
+                continue
+
     return count
 
 
@@ -42,15 +46,15 @@ if __name__ == "__main__":
     source_df = pd.read_csv(args.s, dtype=dtype)
     target_df = pd.read_csv(args.t, dtype=dtype)
 
-    in_source_only = get_missing_records(target_df, source_df['ID'], 'ID')
-    in_target_only = get_missing_records(source_df, target_df['ID'], 'ID')
+    in_target_only = get_missing_records(target_df, source_df['ID'], 'ID')
+    in_source_only = get_missing_records(source_df, target_df['ID'], 'ID')
 
     store = []
     store_missing_records(in_source_only, store, 'Missing in Target')
     store_missing_records(in_target_only, store, 'Missing in Source')
-    total_discrepancies = store_field_discrepancies(source_df, target_df, store, 'ID', 'Field Discrepancy')
-
-
+    total_discrepancies = store_field_discrepancies(
+        source_df, target_df, store, 'ID', 'Field Discrepancy'
+    )
 
     writer = csv.writer(open(args.o, mode='w'))
     writer.writerows(store)
