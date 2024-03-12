d0b94ca44eb5fc1950c4819a43e79a7a35724d66
spaceofmiah <agbonzeosazuwa@gmail.com>
2024-03-12 18:03:19 +0100

    fix: add bash output

--- csv_reconciler.py
+++ csv_reconciler.py
@@ -15,21 +15,24 @@ class ScriptArguments(Tap):
     o: str  
 
 
-def get_missing_records(df, series, key):
+def get_missing_records(df, series, key) -> pd.DataFrame:
     merged = pd.merge(df, series, on=key, how='left', indicator=True)
     data = merged[merged['_merge'] == 'left_only']
     data = data.drop(columns=['_merge'])
     return data
 
-def store_missing_records(df, store, message):
+def store_missing_records(df, store, message) -> None:
     for _, row in df.iterrows():
         store.append((message, *row))
 
-def store_field_discrepancies(source, target, store, key, message):
+def store_field_discrepancies(source, target, store, key, message) -> int:
+    count = 0
     for i, row in source.iterrows():
         for column in source.columns:
             if row[column] != target.loc[i, column] and row[key] == target_df.loc[i, key]:
+                count += 1
                 store.append((message, row[column], target_df.loc[i, column]))
+    return count
 
 
 
@@ -45,10 +48,18 @@ if __name__ == "__main__":
     store = []
     store_missing_records(in_source_only, store, 'Missing in Target')
     store_missing_records(in_target_only, store, 'Missing in Source')
-    store_field_discrepancies(source_df, target_df, store, 'ID', 'Field Discrepancy')
+    total_discrepancies = store_field_discrepancies(source_df, target_df, store, 'ID', 'Field Discrepancy')
+
 
 
     writer = csv.writer(open(args.o, mode='w'))
     writer.writerows(store)
+
+    print("\n\nReconciliation Completed:")
+    print("- Records missing in target: ", in_source_only.shape[0])
+    print("- Records missing in source: ", in_target_only.shape[0])
+    print("- Records with field discrepancies: ", total_discrepancies)
+
+    print("\nReport saved to: ", args.o, "\n\n")
 
 
